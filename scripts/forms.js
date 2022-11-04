a0229611ee019cb37cf5951ce7360b719384c5f6
DanLowo <danlowo@gmail.com>
2022-11-04 09:25:48 +0100

    Form structure

---
+++ scripts/forms.js
@@ -0,0 +1,53 @@
+function transformData(inputArr) {
+  const dataObj = {}
+  for (const input of inputArr) {
+    dataObj[input.name] = input.value
+  }
+
+  return dataObj
+}
+
+function handleFormSubmit(e) {
+  e.preventDefault()
+  const personalDetailsEl = document.getElementById("personal_info")
+  const assetDetailsEl = document.getElementById("asset_details")
+  const spouseAndBeneficiariesDetailsEl = document.getElementById("beneficiaries_details")
+
+  // Handle Person Details Fields
+  const [personalDetailsInputFields, personalDetailsSelectFields] = [
+    personalDetailsEl.querySelectorAll("input"),
+    personalDetailsEl.querySelectorAll("select")
+  ]
+
+  const person_details = transformData(personalDetailsInputFields)
+  Object.assign(person_details, transformData(personalDetailsSelectFields))
+
+
+  // Handle Asset Details Fields
+  const [assetDetailsInputFields, assetDetailsSelectFields] = [
+    assetDetailsEl.querySelectorAll("input"),
+    assetDetailsEl.querySelectorAll("select")
+  ]
+
+  const asset_details = transformData(assetDetailsInputFields)
+  Object.assign(asset_details, transformData(assetDetailsSelectFields))
+
+
+  // Handle Spouse And Beneficiaries Details Fields
+  const [spouseAndBeneficiariesInputFields, spouseAndBeneficiariesDetailsSelectFields] = [
+    spouseAndBeneficiariesDetailsEl.querySelectorAll("input"),
+    spouseAndBeneficiariesDetailsEl.querySelectorAll("select")
+  ]
+
+  const spouse_and_beneficiaries_details = transformData(spouseAndBeneficiariesInputFields)
+  Object.assign(spouse_and_beneficiaries_details, transformData(spouseAndBeneficiariesDetailsSelectFields))
+
+
+  const registrationDetails = {
+    asset_details,
+    person_details,
+    spouse_and_beneficiaries_details
+  }
+
+  console.log(registrationDetails)
+}
